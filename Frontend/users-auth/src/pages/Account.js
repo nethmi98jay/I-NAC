f8575cbe51a3beb4ed55f84164f87efd10d52d1b
nethmi98jay <88782795+nethmi98jay@users.noreply.github.com>
2023-10-22 12:38:00 +0530

    edit

--- Frontend/users-auth/src/pages/Account.js
+++ Frontend/users-auth/src/pages/Account.js
@@ -1,33 +1,11 @@
 import React from 'react'
-import { Link, useNavigate } from 'react-router-dom'
 
-function Navbar() {
-    const isUserSignedIn = !!localStorage.getItem('token')
-    const navigate = useNavigate();
-
-    const handleSignOut = () => {
-        localStorage.removeItem('token')
-        navigate('/login')
-    }
-
+function Account() {
   return (
-    <nav className='flex justify-around p-3 border-b border-zinc-800 items-center bg-[#1a1a1a]/90 text-zinc-300'>
-        <Link to='/'><h1 className='text-3xl'>AuthDB</h1></Link>
-        <ul className='flex gap-6'>
-            {isUserSignedIn ? (
-                <>
-                <Link to='/account'><li>Account</li></Link>
-                <li><button onClick={handleSignOut}>Sign Out</button></li>
-                </>
-            ) : (
-                <>
-                <Link to='/login'><li>Login</li></Link>
-                <Link to='/signup'><li>Signup</li></Link>
-                </>
-            )}
-        </ul>
-    </nav>
+    <div className='w-full h-screen bg-[#1a1a1a] text-white flex justify-center items-center'>
+      <h2 className='text-3xl'>ACCOUNT</h2>
+    </div>
   )
 }
 
-export default Navbar
+export default Account
